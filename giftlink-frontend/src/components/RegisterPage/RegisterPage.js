2d27e72d0e9f53e2016fb3320dfae9bdfc42a4dd
Linden <799693748@qq.com>
2025-02-12 15:52:53 +0800

    implement login and register page, and added routes of the two in /app

---
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import './RegisterPage.css';
+
+function RegisterPage() {
+    // create useState hook variables for firstName, lastName, email, password
+    const [firstName, setFirstName] = useState('firstname');
+    const [lastName, setLastName] = useState('lastname');
+    const [email, setEmail] = useState('email');
+    const [password, setPassword] = useState('password');
+
+    //  create handleRegister function and include console.log(temperary)
+    const handleRegister = async () => {
+        console.log("Register invoked");
+    }
+    return (
+        <div className='container mt-5'>
+            <div className='row justify-content-center'>
+                <div className='col-md-6 clo-lg-4'>
+                    <div className='register-card p-4 border rounder'>
+                        <h2 className='text-center mb-4 font-weight-bold'>Register</h2>
+                        {/* first name, 每个变量有自己的div，div里买呢有label和input，htmlFor关联表单的id*/}
+                        <div className='mb-3'>
+                            <label htmlFor='firstName' className='form-label'>FirstName</label>
+                            <input
+                                id='firstName'
+                                type='text'
+                                className='form-control'
+                                placeholder='Enter your firstname'
+                                value={firstName}
+                                onChange={(e) => setFirstName(e.target.value)}
+                            />
+                        </div>
+                        { /* lastname */}
+                        <div className='mb-3'>
+                            <label htmlFor='lastName' className='form-label'>LastName</label>
+                            <input
+                                id='lastName'
+                                type='text'
+                                className='form-control'
+                                placeholder='Enter your lastname'
+                                value={lastName}
+                                onChange={(e) => setLastName(e.target.value)}
+                            />
+                        </div>
+                        { /* email */}
+                        <div className='mb-3'>
+                            <label htmlFor='email' className='form-label'>Email</label>
+                            <input
+                                id='email'
+                                type='email'
+                                className='form-control'
+                                placeholder='Enter your email'
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                        {/* password */}
+                        <div className='mb-3'>
+                            <label htmlFor='password' className='form-label'>Password</label>
+                            <input
+                                id='password'
+                                type='password'
+                                className='form-control'
+                                placeholder='Enter your password'
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        { /* handleRegister无需传入参数，可以直接绑定 */}
+                        <button className='btn btn-primary w-100 mb-3' onClick={handleRegister}>Register</button>
+                        <p className='mt-4 text-center'>
+                            Already a member? <a href='/app/login' className='text-primary'>Login</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default RegisterPage;
